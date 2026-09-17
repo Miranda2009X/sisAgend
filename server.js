f7fea899f3e928743e8ca520c27b77ec915965e0
leonidas <leonidas.colla@escola.pr.gov.br>
2026-09-17 17:34:14 -0300

    incluso começo do BD

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 const express = require('express');
-const path = require('path');
-const app = require('./src/app');
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'src', 'views')));
-
-app.get('/cliente', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'views', 'cliente.html'));
-});
-
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'views', 'admin.html'));
-});
-
-app.get('/relatorios', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'views', 'relatorios.html'));
-});
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.redirect('/cliente');
+  return res.json({ message: "Servidor do SISAGEND rodando com sucesso! 🚀" });
 });
 
 app.listen(PORT, () => {
-    console.log(`\n🚀 SISAGEND Ativo! Teste nos links abaixo:\n`);
-    console.log(`💻 Tela do Cliente:    http://localhost:${PORT}/cliente`);
-    console.log(`🔧 Painel Admin:      http://localhost:${PORT}/admin`);
-    console.log(`📊 Relatórios TCC:    http://localhost:${PORT}/relatorios\n`);
+  console.log(`[SISAGEND] Servidor online em http://localhost:${PORT}`);
 });
